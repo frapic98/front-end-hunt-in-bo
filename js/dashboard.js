8f14ac73a99e6044f9fac71898c5439093c0b9fa
frapic98 <francescopiconi14@gmail.com>
2022-12-22 18:31:46 +0100

    Fix PieChart

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,11 @@
+var nfontanelle=1 ;
+var npanchine=1 ;
+var nbagni=1;
+var nparchi=1 ;
+var ncestini=   1;
+var ndefribillatori=1 ;
 $(document).ready(function() {
+
     // UTENTI
    /*$.get( "https://hunt-in-bo.herokuapp.com/user")
     .done(function( data ) {
@@ -33,10 +40,9 @@ $(document).ready(function() {
             }
         });
 
+    });
 
-
-        $.ajax
-        ({
+        var url=$.ajax({
             type: "GET",
             url: "https://hunt-in-bo.herokuapp.com/poi",
             dataType: 'json',
@@ -45,39 +51,43 @@ $(document).ready(function() {
                 console.log(npoi);
                 document.getElementById("npoi").innerHTML = npoi.length;
                 //count the number of each category
-                var nfontanelle = 0;
-                var npanchine = 0;
-                var nbagni = 0;
-                var nparchi = 0;
-                var ncestini = 0;
-                var ndefribillatori= 0;
-                var naltro = 0;
                 for (var i = 0; i < npoi.length; i++) {
-                    var obj = npoi[i];
-                    if (obj.category == 1) {
+                    if (npoi[i].category == 1) {
                         nfontanelle++;
                     }
-                    else if (obj.category == 2) {
+                    if (npoi[i].category == 2) {
                         npanchine++;
                     }
-                    else if (obj.category == 3) {
+                    if (npoi[i].category == 3) {
                         nbagni++;
                     }
-                    else if (obj.category == 4) {
+                    if (npoi[i].category == 4) {
                         nparchi++;
                     }
-                    else if (obj.category == 5) {
+                    if (npoi[i].category == 5) {
                         ncestini++;
                     }
-                    else if (obj.category == 6) {
+                    if (npoi[i].category == 6) {
                         ndefribillatori++;
                     }
                 }
-                console.log(nfontanelle);
-                
+                myPieChart.config.data.datasets[0].data[0]=nfontanelle;
+                myPieChart.config.data.datasets[0].data[1]=npanchine;
+                myPieChart.config.data.datasets[0].data[2]=nbagni;
+                myPieChart.config.data.datasets[0].data[3]=nparchi;
+                myPieChart.config.data.datasets[0].data[4]=ncestini;
+                myPieChart.config.data.datasets[0].data[5]=ndefribillatori;
+
+                myPieChart.update();
+             
             }
-        });
-      
+        
+        })
+
+
+    
+
+console.log(nfontanelle);
 
     // CORSI
    /* $.get( "export.geojson")
@@ -123,10 +133,48 @@ $(document).ready(function() {
             alert("Si è verificato un errore nell'ottenere i dati dell'associazione frequentare")
         });*/
 
-});
 
 
 function logout() {
     localStorage.removeItem("jwt");
     window.location.href = './login.html'
 }
+
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+console.log(nfontanelle);
+console.log(npanchine);
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Fontanelle", "Panchine", "Bagni", "Parchi", "Cestini", "Defribillatori"],
+    datasets: [{
+      data: [nfontanelle, npanchine, nbagni, nparchi, ncestini, ndefribillatori],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#e74a3b', '#858796', '#f6c23e'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#e74a3b', '#858796', '#f6c23e'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+console.log(myPieChart);
